a3cf597bb95287dce3fe503681aff641c97233ff
LZugar <59911931+LZugar@users.noreply.github.com>
2022-04-18 20:17:39 +0200

    Changed API call used
    Current account has no acces to 16 day preview API call so different APi call is implemented but not working yet

--- src/Search.js
+++ src/Search.js
@@ -6,17 +6,22 @@ import Forecast from "./Forecast";
 export default function Search() {
   let [city, setCity] = useState("");
   let [text, setText] = useState("");
+  let [forecast, setForecast] = useState("");
   let APIkey = "503764422e28613a3aa4cb8119955a59";
 
   function getWeather(response) {
-    console.log(response.data);
+      console.log(response);
     setText(<MakeList data={response.data} />);
+
+    setForecast(<Forecast data={response.data}/>)
   }
   function handleQuery(event) {
     event.preventDefault();
-    console.log(`The city is ${city}`)
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}&units=metric`;
+    if(city){
+      let url = `https://api.openweathermap.org/data/2.5/onecall?q=${city}&units=metric&exclude=minutely,hourly&appid=${APIkey}`; 
+      console.log(url);
       axios.get(url).then(getWeather);
+    }
   }
   function updateQuery(event) {
     setCity(event.target.value);
@@ -28,7 +33,7 @@ export default function Search() {
         <input type="submit" />
       </form>
       <div className='CurrentWeather'>{text}</div>
-      <div className='ForecastTiles'><Forecast city={city} days={3} /></div>
+      <div className='ForecastTiles'>{forecast}</div>
     </div>
   );
 }
